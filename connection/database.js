0c9aa39816dbdf20a2e09847f3d284b139c7b3c6
koiseee <detorresmhel02@gmail.com>
2024-05-28 23:16:31 +0800

    mm

--- connection/database.js
+++ connection/database.js
@@ -1,14 +1,16 @@
 const Sequelize = require("sequelize");
 const dotenv = require("dotenv");
+const mysql2 = require("mysql2");
 
 dotenv.config();
 
 const sequelizeConnect = new Sequelize({
-    dialect: "mysql",
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-})  
+  dialect: "mysql",
+  dialectModule: mysql2,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+});
 
 module.exports = sequelizeConnect;
