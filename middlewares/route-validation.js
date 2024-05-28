db4ddc7b17798e18d8bc39af1617b414f35d2d9e
DevKiane <descbls@gmail.com>
2024-05-28 11:08:18 +0800

    json body parser

--- middlewares/route-validation.js
+++ middlewares/route-validation.js
@@ -1,15 +1,15 @@
-const { validationResult } = require("express-validator");
+const { validationResult } = require('express-validator');
 
-module.exports = (req, res, next) => {
+const validation = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    if (req.files && req.files.eventOrganizerProfile) {
-      const eventOrganizerProfile = req.files.eventOrganizerProfile[0];
-    }
-    const error = new Error("An error occured! Detailed explanation provided.");
-    error.statusCode = 422;
-    error.data = errors.array();
-    throw error;
+    return res.status(400).json({
+      success: false,
+      message: "Validation errors occurred",
+      data: errors.array()
+    });
   }
   next();
 };
+
+module.exports = validation;
